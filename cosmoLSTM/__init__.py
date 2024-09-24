0da95820265b2797de223890545d2e548d7bc011
Johnny Dorigo Jones <89427403+jdorigojones@users.noreply.github.com>
2024-09-24 12:09:38 -0600

    Update __init__.py

--- cosmoLSTM/__init__.py
+++ cosmoLSTM/__init__.py
@@ -4,14 +4,14 @@ __author__ = "Johnny Dorigo Jones"
 from pathlib import Path
 
 HERE = __file__[: -len("__init__.py")]
-if not Path(HERE + "dataset_21cmLSTM.h5").exists():
+if not Path(HERE + "dataset_21cmGEM_VAE.h5").exists():
     import requests
 
-    print("Downloading 21cmGEM data set (see Cohen et al. 2020).")
+    print("Downloading data set for 21cmGEM (Cohen et al. 2020) used in Dorigo Jones et al. 2024 and also Bye et al. 2022 and Bevins et al. 2021.")
     r = requests.get(
         "https://zenodo.org/record/5084114/files/dataset_21cmVAE.h5?download=1"
     )
-    with open(HERE + "dataset_21cmLSTM.h5", "wb") as f:
+    with open(HERE + "dataset_21cmGEM_VAE.h5", "wb") as f:
         f.write(r.content)
 
 from cosmoLSTM import emulator
