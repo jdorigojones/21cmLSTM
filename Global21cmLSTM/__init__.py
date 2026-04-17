aea3cb17f971447c9f6d3425596d8e032f29fd98
Johnny Dorigo Jones <89427403+jdorigojones@users.noreply.github.com>
2026-04-16 22:12:47 -0600

    Update __init__.py

--- Global21cmLSTM/__init__.py
+++ Global21cmLSTM/__init__.py
@@ -14,7 +14,11 @@ from Global21cmLSTM import __path__
 
 from Global21cmLSTM import emulator_21cmGEM
 from Global21cmLSTM import emulator_ARES
+from Global21cmLSTM import emulator_foreground_LST1
 from Global21cmLSTM import emulator_foreground_LST2
+from Global21cmLSTM import emulator_foreground_LST3
+from Global21cmLSTM import emulator_foreground_LST4
+from Global21cmLSTM import emulator_foreground_LST5
 from Global21cmLSTM import preprocess_21cmGEM
 from Global21cmLSTM import preprocess_ARES
 from Global21cmLSTM import preprocess_foreground
